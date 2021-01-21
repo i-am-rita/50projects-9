be2e935538ab2f38a7506159090754a6c37b32cf
Rita <ritauzoma07@gmail.com>
2021-01-21 20:24:05 +0100

    initial commit to github

---
+++ script.js
@@ -0,0 +1,26 @@
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+
+// loop through
+sounds.forEach((sound) => {
+  // create buttons
+  const btn = document.createElement('button')
+  btn.classList.add('btn')
+
+  btn.innerText = sound
+
+  btn.addEventListener('click' () => {
+    stopSongs()
+    document.getElementById(sound).play()
+  })
+
+  document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs(){
+  sounds.forEach(sound => {
+    const song =   document.getElementById(sound)
+
+    song.onpause()
+    song.currentTime = 0;
+  })
+}
